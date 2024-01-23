705950d71854ada93311ee01ab164ae5f1afd617
evarayHEIG <eva.ray@heig-vd.ch>
2024-01-23 20:32:15 +0100

    Add timer in Musician app

--- docker/image-musician/src/main/java/musician/MusicianMulticastSender.java
+++ docker/image-musician/src/main/java/musician/MusicianMulticastSender.java
@@ -4,7 +4,11 @@ import java.io.IOException;
 import java.net.DatagramSocket;
 import java.net.DatagramPacket;
 import java.net.InetSocketAddress;
+import java.util.Timer;
+import java.util.TimerTask;
+
 import static java.nio.charset.StandardCharsets.*;
+
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
@@ -21,18 +25,40 @@ public class MusicianMulticastSender {
         String message = gson.toJson(musician);
         InetSocketAddress dest_address = new InetSocketAddress(IPADDRESS, PORT);
 
-        while (true) {
-            try (DatagramSocket socket = new DatagramSocket()) {
-
-                System.out.println(message);
-                byte[] payload = message.getBytes(UTF_8);
-                DatagramPacket packet = new DatagramPacket(payload, payload.length, dest_address);
-                socket.send(packet);
-                Thread.sleep(1000);
-            } catch (Exception ex) {
-                System.out.println(ex.getMessage());
+        Timer timer = new Timer();
+        timer.scheduleAtFixedRate(new TimerTask() {
+            @Override
+            public void run() {
+                try (DatagramSocket socket = new DatagramSocket()) {
+                    System.out.println(message);
+                    byte[] payload = message.getBytes(UTF_8);
+                    DatagramPacket packet = new DatagramPacket(payload, payload.length, dest_address);
+                    socket.send(packet);
+                } catch (IOException ex) {
+                    System.out.println(ex.getMessage());
+                }
             }
-        }
+        }, 0, 1000);
     }
 
 }
+
+/* TODO: pourquoi version avc ScheduledExecutorService ne fonctionne pas ?
+
+try (DatagramSocket socket = new DatagramSocket(); ScheduledExecutorService executor = Executors.newSingleThreadScheduledExecutor()) {
+            System.out.println("Sending message: " + message + " to " + destAddress.getAddress() + ", port " + destAddress.getPort());
+
+            executor.scheduleAtFixedRate(() -> {
+                try {
+                    byte[] payload = message.getBytes(UTF_8);
+                    DatagramPacket packet = new DatagramPacket(payload, payload.length, destAddress);
+                    socket.send(packet);
+                    System.out.println(message);
+                } catch (IOException ex) {
+                    System.out.println(ex.getMessage());
+                }
+            }, 0, 1, java.util.concurrent.TimeUnit.SECONDS);
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage());
+        }
+*/
